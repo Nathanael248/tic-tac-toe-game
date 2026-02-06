72bd1ee36079e052a54872b2c76dad7ff4de34f3
Nathanael248 <okunadenathaniel@gmail.com>
2026-02-06 13:44:59 +0000

    Game now stops when a player wins.

--- GitHub/tic-tac-toe-game/tic-tac-toe.js
+++ GitHub/tic-tac-toe-game/tic-tac-toe.js
@@ -1,4 +1,5 @@
 let board = ["", "", "", "", "", "", "", "", ""]; // Represents the Tic Tac Toe board
+let gameActive = true; // Track if the game is still active
 
 function isCellOccupied(buttonId, move) {
   // Function to check if a cell is already occupied
@@ -15,6 +16,8 @@ function checkGame(playerMove) {
   ) {
     document.querySelector(".js-display-result").innerHTML =
       `We now have a winner!: ${playerMove}`;
+    gameActive = false;
+    return true;
   } else if (
     board[3] === playerMove &&
     board[4] === playerMove &&
@@ -22,6 +25,8 @@ function checkGame(playerMove) {
   ) {
     document.querySelector(".js-display-result").innerHTML =
       `We now have a winner!: ${playerMove}`;
+    gameActive = false;
+    return true;
   } else if (
     board[6] === playerMove &&
     board[7] === playerMove &&
@@ -29,6 +34,8 @@ function checkGame(playerMove) {
   ) {
     document.querySelector(".js-display-result").innerHTML =
       `We now have a winner!: ${playerMove}`;
+    gameActive = false;
+    return true;
   } else if (
     board[0] === playerMove &&
     board[3] === playerMove &&
@@ -36,6 +43,8 @@ function checkGame(playerMove) {
   ) {
     document.querySelector(".js-display-result").innerHTML =
       `We now have a winner!: ${playerMove}`;
+    gameActive = false;
+    return true;
   } else if (
     board[1] === playerMove &&
     board[4] === playerMove &&
@@ -43,6 +52,8 @@ function checkGame(playerMove) {
   ) {
     document.querySelector(".js-display-result").innerHTML =
       `We now have a winner!: ${playerMove}`;
+    gameActive = false;
+    return true;
   } else if (
     board[2] === playerMove &&
     board[5] === playerMove &&
@@ -50,6 +61,8 @@ function checkGame(playerMove) {
   ) {
     document.querySelector(".js-display-result").innerHTML =
       `We now have a winner!: ${playerMove}`;
+    gameActive = false;
+    return true;
   } else if (
     board[0] === playerMove &&
     board[4] === playerMove &&
@@ -57,6 +70,8 @@ function checkGame(playerMove) {
   ) {
     document.querySelector(".js-display-result").innerHTML =
       `We now have a winner!: ${playerMove}`;
+    gameActive = false;
+    return true;
   } else if (
     board[2] === playerMove &&
     board[4] === playerMove &&
@@ -64,6 +79,8 @@ function checkGame(playerMove) {
   ) {
     document.querySelector(".js-display-result").innerHTML =
       `We now have a winner!: ${playerMove}`;
+    gameActive = false;
+    return true;
   } else if (
     board[0] &&
     board[1] &&
@@ -76,73 +93,90 @@ function checkGame(playerMove) {
     board[8]
   ) {
     document.querySelector(".js-display-result").innerHTML = `It's a draw!`;
+    gameActive = false;
   } else {
     document.querySelector(".js-display-result").innerHTML = "Keep on playing";
   }
 }
-// console.log(board[1], board[2], board[0]);
-// if (board[1] && board[2] && board[0] === "X") {
-//   console.log("working perfectly");
-// }
+
 let playerMove = "X";
 
 document.querySelector(`.js-cell-0`).addEventListener("click", () => {
+  if (!gameActive) return;
+
   let move = document.querySelector(`.js-cell-0`);
   const buttonId = document.querySelector(".js-cell-0").dataset.boxNo;
 
   if (isCellOccupied(buttonId, move)) {
+    checkGame(playerMove);
+    return;
+  }
+
+  if (checkGame(playerMove) === true) {
     return;
   }
   move.innerHTML = `<img class="move" src="${playerMove}.png" />`;
-  checkGame(playerMove);
   board[buttonId] = playerMove;
-
   playerMove = playerMove === "X" ? (playerMove = "O") : (playerMove = "X");
 });
 
 document.querySelector(`.js-cell-1`).addEventListener("click", () => {
+  if (!gameActive) return;
+
   let move = document.querySelector(`.js-cell-1`);
   const buttonId = document.querySelector(".js-cell-1").dataset.boxNo;
   if (isCellOccupied(buttonId, move)) {
     return;
   }
+
+  if (checkGame(playerMove) === true) {
+    return;
+  }
   move.innerHTML = `<img class="move" src="${playerMove}.png" />`;
 
   board[buttonId] = playerMove;
-  checkGame(playerMove);
-
   playerMove = playerMove === "X" ? (playerMove = "O") : (playerMove = "X");
 });
 
 document.querySelector(`.js-cell-2`).addEventListener("click", () => {
+  if (!gameActive) return;
+
   let move = document.querySelector(`.js-cell-2`);
   const buttonId = document.querySelector(".js-cell-2").dataset.boxNo;
   if (isCellOccupied(buttonId, move)) {
     return;
   }
-  move.innerHTML = `<img class="move" src="${playerMove}.png" />`;
 
-  board[buttonId] = playerMove;
-  checkGame(playerMove);
-
-  playerMove = playerMove === "X" ? (playerMove = "O") : (playerMove = "X");
-});
-
-document.querySelector(`.js-cell-3`).addEventListener("click", () => {
-  let move = document.querySelector(`.js-cell-3`);
-  const buttonId = document.querySelector(".js-cell-3").dataset.boxNo;
-  if (isCellOccupied(buttonId, move)) {
+  if (checkGame(playerMove) === true) {
     return;
   }
   move.innerHTML = `<img class="move" src="${playerMove}.png" />`;
 
   board[buttonId] = playerMove;
-  checkGame(playerMove);
+  playerMove = playerMove === "X" ? (playerMove = "O") : (playerMove = "X");
+});
 
+document.querySelector(`.js-cell-3`).addEventListener("click", () => {
+  if (!gameActive) return;
+
+  let move = document.querySelector(`.js-cell-3`);
+  const buttonId = document.querySelector(".js-cell-3").dataset.boxNo;
+  checkGame(playerMove);
+  if (isCellOccupied(buttonId, move)) {
+    return;
+  }
+  if (checkGame(playerMove) === true) {
+    return;
+  }
+  move.innerHTML = `<img class="move" src="${playerMove}.png" />`;
+
+  board[buttonId] = playerMove;
   playerMove = playerMove === "X" ? (playerMove = "O") : (playerMove = "X");
 });
 
 document.querySelector(`.js-cell-4`).addEventListener("click", () => {
+  if (!gameActive) return;
+
   let move = document.querySelector(`.js-cell-4`);
   const buttonId = document.querySelector(".js-cell-4").dataset.boxNo;
   if (isCellOccupied(buttonId, move)) {
@@ -151,12 +185,15 @@ document.querySelector(`.js-cell-4`).addEventListener("click", () => {
   move.innerHTML = `<img class="move" src="${playerMove}.png" />`;
 
   board[buttonId] = playerMove;
-  checkGame(playerMove);
-
+  if (checkGame(playerMove) === true) {
+    return;
+  }
   playerMove = playerMove === "X" ? (playerMove = "O") : (playerMove = "X");
 });
 
 document.querySelector(`.js-cell-5`).addEventListener("click", () => {
+  if (!gameActive) return;
+
   let move = document.querySelector(`.js-cell-5`);
   const buttonId = document.querySelector(".js-cell-5").dataset.boxNo;
   if (isCellOccupied(buttonId, move)) {
@@ -165,12 +202,15 @@ document.querySelector(`.js-cell-5`).addEventListener("click", () => {
   move.innerHTML = `<img class="move" src="${playerMove}.png" />`;
 
   board[buttonId] = playerMove;
-  checkGame(playerMove);
-
+  if (checkGame(playerMove) === true) {
+    return;
+  }
   playerMove = playerMove === "X" ? (playerMove = "O") : (playerMove = "X");
 });
 
 document.querySelector(`.js-cell-6`).addEventListener("click", () => {
+  if (!gameActive) return;
+
   let move = document.querySelector(`.js-cell-6`);
   const buttonId = document.querySelector(".js-cell-6").dataset.boxNo;
   if (isCellOccupied(buttonId, move)) {
@@ -179,12 +219,15 @@ document.querySelector(`.js-cell-6`).addEventListener("click", () => {
   move.innerHTML = `<img class="move" src="${playerMove}.png" />`;
 
   board[buttonId] = playerMove;
-  checkGame(playerMove);
-
+  if (checkGame(playerMove) === true) {
+    return;
+  }
   playerMove = playerMove === "X" ? (playerMove = "O") : (playerMove = "X");
 });
 
 document.querySelector(`.js-cell-7`).addEventListener("click", () => {
+  if (!gameActive) return;
+
   let move = document.querySelector(`.js-cell-7`);
   const buttonId = document.querySelector(".js-cell-7").dataset.boxNo;
   if (isCellOccupied(buttonId, move)) {
@@ -193,12 +236,15 @@ document.querySelector(`.js-cell-7`).addEventListener("click", () => {
   move.innerHTML = `<img class="move" src="${playerMove}.png" />`;
 
   board[buttonId] = playerMove;
-  checkGame(playerMove);
-
+  if (checkGame(playerMove) === true) {
+    return;
+  }
   playerMove = playerMove === "X" ? (playerMove = "O") : (playerMove = "X");
 });
 
 document.querySelector(`.js-cell-8`).addEventListener("click", () => {
+  if (!gameActive) return;
+
   let move = document.querySelector(`.js-cell-8`);
   const buttonId = document.querySelector(".js-cell-8").dataset.boxNo;
   if (isCellOccupied(buttonId, move)) {
@@ -207,7 +253,8 @@ document.querySelector(`.js-cell-8`).addEventListener("click", () => {
   move.innerHTML = `<img class="move" src="${playerMove}.png" />`;
 
   board[buttonId] = playerMove;
-  checkGame(playerMove);
-
+  if (checkGame(playerMove) === true) {
+    return;
+  }
   playerMove = playerMove === "X" ? (playerMove = "O") : (playerMove = "X");
 });
